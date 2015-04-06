61dad28730ef0f957f7c561d1c1f721729539b6f
Dmitry Ponyatov <dponyatov@gmail.com>
2015-04-06 18:12:12 +0400

    try 53 output
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- SDio/CardInfo/CardInfo.ino
+++ SDio/CardInfo/CardInfo.ino
@@ -49,7 +49,7 @@ void setup()
   // (10 on most Arduino boards, 53 on the Mega) must be left as an output
   // or the SD library functions will not work.
   pinMode(10, OUTPUT);     // change this to 53 on a mega
-
+  pinMode(53, OUTPUT);     // change this to 53 on a mega
 
   // we'll use the initialization code from the utility libraries
   // since we're just testing if the card is working!
