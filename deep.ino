49b8be75393c607c0ff929debb367b82ee20ba2d
Dmitry Ponyatov <dponyatov@gmail.com>
2015-04-12 22:04:55 +0400

    * SD init seq. first CMD0s fail on no SD in slot
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- deep.ino
+++ deep.ino
@@ -9,35 +9,103 @@ static const uint8_t MISO = 50;
 static const uint8_t SCK  = 52;
 */
 
-void setup() {
-  // put your setup code here, to run once:
-  SPI.begin();
-  SPI.setClockDivider(SPI_CLOCK_DIV128);
-/*
-  pinMode(SS, OUTPUT);
-  digitalWrite(SS,HIGH);
+void SPI_preinit() {
+  pinMode(SS, OUTPUT); // SS#
   pinMode(MOSI, OUTPUT);
   pinMode(MISO,  INPUT);
   pinMode(SCK, OUTPUT);
-*/
+  // trigger pulse
+  digitalWrite(SS,LOW);
+}
+
+void SPI_postinit() {  
+  SPI.begin();
+  SPI.setDataMode(SPI_MODE0); // required for SD/MMC
+  SPI.setClockDivider(SPI_CLOCK_DIV128);
+}
+
+#define SD_CMD0 0x00
+#define SD_GO_IDLE_STATE SD_CMD0
+// arg:no resp:R1 data: no
+
+struct T_SD_R1 {
+  uint8_t pad0:1; // always 0
+  uint8_t ParameterErr:1;
+  uint8_t AddressErr:1;
+  uint8_t EraseSeqErr:1;
+  uint8_t CommandCRCErr:1;
+  uint8_t IllegalCommand:1;
+  uint8_t EraseReset:1;
+  uint8_t InIdleState:1;
+} __attribute__ ((__packed__));
+
+union { uint8_t byte; T_SD_R1 bit; } SD_R1;
+
+struct SD_R2 {
+  
+};
+
+struct SD_R3 {
+  T_SD_R1 R1;
+} __attribute__ ((__packed__)) SD_R3;
+
+struct SD_R7 {
+  
+};
+
+void SD_init() {
+  SPI_preinit();
+  // do SPI mode setup @ http://elm-chan.org/docs/mmc/gx1/sdinit.png
+  Serial.print("\nSD init:");
+  // wail >1ms on power on
+  delay(1);
+  // dummy clock CS=DI=high
+  digitalWrite(SS,HIGH); digitalWrite(MOSI,HIGH);
+  for (uint8_t i=0;i<77;i++) { digitalWrite(SCK,LOW); digitalWrite(SCK,HIGH); }
+  // SPI enable
+  SPI_postinit(); SD_start();
+  // cmd0
+  Serial.print(" CMD0:");
+  SD_R1.byte=SPI.transfer(SD_CMD0);
+  uint8_t cmd0t1=SD_R1.byte;
+  Serial.print(SD_R1.byte);
+  Serial.print(" CMD0:");
+  SD_R1.byte=SPI.transfer(SD_CMD0);
+  uint8_t cmd0t2=SD_R1.byte;
+  Serial.print(SD_R1.byte);
+  if (cmd0t1==0xFF & cmd0t2==0xFF) { 
+    Serial.print(" \bError: no card");
+    SD_error();
+  }
+  Serial.println("\n");
 }
 
 void SD_start() {
-  digitalWrite(SS, LOW);
-}
-
-void SD_stop() {
   digitalWrite(SS,HIGH);
 }
 
+void SD_stop() {
+  digitalWrite(SS, LOW);
+}
+
+void SD_error() {
+  SD_stop();
+  SPI.end();
+  SD_stop();
+}
+
+void setup() {
+  Serial.begin(115200);
+  SD_init();
+}
+
 void loop() {
-  // put your main code here, to run repeatedly:
-//  shiftOut(MOSI,SCK,LSBFIRST,0xA5);
-//  shiftOut(MOSI,SCK,LSBFIRST,0x00);
-//  shiftOut(MOSI,SCK,LSBFIRST,0xFF);
+  for (;;) delay(111);
+/*
   SD_start();
   SPI.transfer(0x00);
   SD_stop();
   SPI.transfer(0xFF);
+*/
 }
 
